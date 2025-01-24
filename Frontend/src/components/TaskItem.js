371aa2c428dca7fbabbcaf1df5d7afce3fd33f11
tejanarra <snarra@hawk.iit.edu>
2025-01-23 20:48:40 -0500

    Update TaskItem.js

--- Frontend/src/components/TaskItem.js
+++ Frontend/src/components/TaskItem.js
@@ -263,7 +263,6 @@ const TaskItem = ({
         message="Are you sure you want to delete this task? This action cannot be undone."
         onConfirm={confirmDelete}
         onCancel={() => setShowDeleteModal(false)}
-
       />
     </>
   );
